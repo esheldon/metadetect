827ac49a092dd226bdb22c934dfb8978393ce87d
Matthew R Becker <becker.mr@gmail.com>
2018-12-28 14:16:33 -0600

    script to plot the psf

---
+++ matts_simple_sims/v9/plot_psf.py
@@ -0,0 +1,80 @@
+import numpy as np
+import galsim
+import galsim.des
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from fit_des_psf import ShpPSF
+
+sns.set()
+
+psf = ShpPSF('shp_psf.fit')
+# psf = galsim.des.DES_PSFEx('psfcat.psf', wcs=galsim.PixelScale(0.263))
+
+y, x = np.mgrid[0:1000:50, 0:1000:50]
+x = x.ravel()
+y = y.ravel()
+
+sigma = []
+fwhm = []
+g1 = []
+g2 = []
+
+for _x, _y in zip(x, y):
+    img = psf.getPSF(galsim.PositionD(x=_x, y=_y)).drawImage(
+        nx=33,
+        ny=33,
+        scale='0.263')
+    admom = galsim.hsm.FindAdaptiveMom(img)
+    g1.append(admom.observed_shape.g1)
+    g2.append(admom.observed_shape.g2)
+    sigma.append(admom.moments_sigma)
+    fwhm.append(img.calculateFWHM())
+
+sigma = np.array(sigma)
+g1 = np.array(g1)
+g2 = np.array(g2)
+fwhm = np.array(fwhm)
+
+tcks = np.arange(20) * 50 + 25
+
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
+
+ax = axs[0, 0]
+sns.heatmap(
+    fwhm.reshape(20, 20),
+    xticklabels=tcks,
+    yticklabels=tcks,
+    square=True,
+    ax=ax)
+ax.set_title('FWHM [arcsec]')
+
+ax = axs[0, 1]
+sns.heatmap(
+    sigma.reshape(20, 20),
+    xticklabels=tcks,
+    yticklabels=tcks,
+    square=True,
+    ax=ax)
+ax.set_title('ADMOM sigma')
+
+
+ax = axs[1, 0]
+sns.heatmap(
+    g1.reshape(20, 20),
+    xticklabels=tcks,
+    yticklabels=tcks,
+    square=True,
+    ax=ax)
+ax.set_title('g1')
+
+ax = axs[1, 1]
+sns.heatmap(
+    g2.reshape(20, 20),
+    xticklabels=tcks,
+    yticklabels=tcks,
+    square=True,
+    ax=ax)
+ax.set_title('g2')
+
+plt.show()
