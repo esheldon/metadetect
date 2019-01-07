518d7de1633ac7af722b6a39548078332420cd61
Matthew R Becker <becker.mr@gmail.com>
2019-01-07 16:35:51 -0600

    logs to temp

--- matts_simple_sims/v15/job_condor.sh
+++ matts_simple_sims/v15/job_condor.sh
@@ -2,10 +2,24 @@
 
 export OMP_NUM_THREADS=1
 
+if [[ -n $_CONDOR_SCRATCH_DIR ]]; then
+    # the condor system creates a scratch directory for us,
+    # and cleans up afterward
+    tmpdir=$_CONDOR_SCRATCH_DIR
+    export TMPDIR=$tmpdir
+else
+    # otherwise use the TMPDIR
+    tmpdir=$TMPDIR
+    mkdir -p $tmpdir
+fi
+
 source activate bnl
 
 echo `which python`
 
 # about 1 to 1.6 hours per job
 # args are nsims, seed, odir
-python run_sim_condor.py $1 $2 $3 >& $3/logs/log_$2.oe
+python run_sim_condor.py $1 $2 $3 >& ${tmpdir}/log_$2.oe
+
+mkdir -p $3/logs
+mv ${tmpdir}/log_$2.oe $3/logs/.
