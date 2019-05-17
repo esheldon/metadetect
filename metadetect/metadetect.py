06d0ec802d9dc0292f732ef5b2df01e72555980a
Erin Sheldon <erin.sheldon@gmail.com>
2019-05-17 07:43:29 -0400

    store fits for the psf in the primary observation
    also some code cleanup for flake8

--- metadetect/metadetect.py
+++ metadetect/metadetect.py
@@ -1,22 +1,26 @@
 import ngmix
+from ngmix.gexceptions import BootPSFFailure
 import esutil as eu
 from . import detect
 from . import fitting
-from . import defaults
+from . import procflags
+
 
 def do_metadetect(config, mbobs, rng):
     """
-    meta-detect on the multi-band observations.
+    meta-detect on the multi-band observations.  For parameters, see
+    docs on the Metadetect class
     """
-    md=Metadetect(config, mbobs, rng)
+    md = Metadetect(config, mbobs, rng)
     md.go()
     return md.result
+
 
 class Metadetect(dict):
     """
     meta-detect on the multi-band observations.
 
-    parameters
+    Parameters
     ----------
     config: dict
         Configuration dictionary. Possible entries are
@@ -26,12 +30,18 @@ class Metadetect(dict):
             fofs (if running MOF)
             mof (if running MOF)
 
+    mbobs: ngmix.MultiBandObsList
+        We will do detection and measurements on these images
+    rng: numpy.random.RandomState
+        Random number generator
     """
     def __init__(self, config, mbobs, rng):
         self._set_config(config)
-        self.mbobs=mbobs
+        self.mbobs = mbobs
         self.nband = len(mbobs)
-        self.rng=rng
+        self.rng = rng
+
+        self._fit_original_psfs()
 
         self._set_fitter()
 
@@ -41,27 +51,27 @@ class Metadetect(dict):
         get the result dict, keyed by the metacal type such
         as 'noshear', '1p', '1m', '2p', '2m'
         """
-        if not hasattr(self,'_result'):
+        if not hasattr(self, '_result'):
             raise RuntimeError('run go() first')
 
         return self._result
 
     def go(self):
         """
-        make sheared versions of the images, run detection and measurements on each
+        make sheared versions of the images, run detection and measurements on
+        each
         """
-        odict=self._get_all_metacal()
+        odict = self._get_all_metacal()
 
         self._result = {}
         for key, mbobs in odict.items():
             self._result[key] = self._measure(mbobs)
 
-
     def _set_fitter(self):
         """
         set the fitter to be used
         """
-        self._fitter=fitting.Moments(
+        self._fitter = fitting.Moments(
             self,
             self.rng,
         )
@@ -72,24 +82,30 @@ class Metadetect(dict):
         detection as well as measurements
         """
 
-        mer = self._do_detect(mbobs)
-        mbm=mer.get_multiband_meds()
+        medsifier = self._do_detect(mbobs)
+        mbm = medsifier.get_multiband_meds()
 
         mbobs_list = mbm.get_mbobs_list()
 
-        res=self._fitter.go(mbobs_list)
+        res = self._fitter.go(mbobs_list)
 
-        res = self._add_positions(mer.cat, res)
+        res = self._add_positions_and_psf(medsifier.cat, res)
         return res
 
-    def _add_positions(self, cat, res):
+    def _add_positions_and_psf(self, cat, res):
         """
         add catalog positions to the result
         """
+
+        res['psfrec_flags'][:] = self.psf_stats['flags']
+        res['psfrec_g'][:, 0] = self.psf_stats['g1']
+        res['psfrec_g'][:, 1] = self.psf_stats['g2']
+        res['psfrec_T'][:] = self.psf_stats['T']
+
         if cat.size > 0:
             new_dt = [
-                ('sx_row','f4'),
-                ('sx_col','f4'),
+                ('sx_row', 'f4'),
+                ('sx_col', 'f4'),
             ]
             newres = eu.numpy_util.add_fields(
                 res,
@@ -107,23 +123,16 @@ class Metadetect(dict):
         """
         use a MEDSifier to run detection
         """
-        mer=detect.MEDSifier(
+        return detect.MEDSifier(
             mbobs,
             sx_config=self['sx'],
             meds_config=self['meds'],
         )
-        return mer
-
-
 
     def _get_all_metacal(self):
         """
         get the sheared versions of the observations
         """
-
-        if self['metacal'].get('symmetrize_psf',False):
-            assert 'psf' in self,'need psf fitting for symmetrize_psf'
-            fitting.fit_all_psfs(self.mbobs, self['psf'], self.rng)
 
         odict = ngmix.metacal.get_all_metacal(
             self.mbobs,
@@ -145,3 +154,51 @@ class Metadetect(dict):
             'sx setting must be present in config'
         assert 'meds' in self, \
             'meds setting must be present in config'
+
+    def _fit_original_psfs(self):
+        """
+        fit the original psfs and get the mean g1,g2,T across
+        all bands
+
+        This can fail and flags will be set, but we proceed
+        """
+
+        try:
+            fitting.fit_all_psfs(self.mbobs, self['psf'], self.rng)
+
+            g1sum = 0.0
+            g2sum = 0.0
+            Tsum = 0.0
+            wsum = 0.0
+
+            for obslist in self.mbobs:
+                for obs in obslist:
+                    wt = obs.weight.max()
+                    g1, g2, T = obs.psf.gmix.get_g1g2T()
+
+                    g1sum += g1*wt
+                    g2sum += g2*wt
+                    Tsum += T*wt
+                    wsum += wt
+
+            if wsum <= 0.0:
+                raise BootPSFFailure('zero weights, could not '
+                                     'get mean psf properties')
+            g1 = g1sum/wsum
+            g2 = g2sum/wsum
+            T = Tsum/wsum
+
+            flags = 0
+
+        except BootPSFFailure:
+            flags = procflags.PSF_FAILURE
+            g1 = -9999.0
+            g2 = -9999.0
+            T = -9999.0
+
+        self.psf_stats = {
+            'flags': flags,
+            'g1': g1,
+            'g2': g2,
+            'T': T,
+        }
