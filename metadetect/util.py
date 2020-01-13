65f9095cd3979b20d650872939a898af0a2fed01
Erin Sheldon <erin.sheldon@gmail.com>
2020-01-13 08:47:49 -0500

    add some util for lsst sim stuff

--- metadetect/util.py
+++ metadetect/util.py
@@ -1,3 +1,9 @@
+import logging
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
 class Namer(object):
     """
     create strings with a specified front prefix
@@ -25,3 +31,66 @@ class Namer(object):
                 n = '%s_%s' % (n, self.back)
 
         return n
+
+
+def trim_odd_image(im):
+    """
+    trim an odd dimension image to by square and with equal distance from
+    canonical center to all edges
+    """
+
+    dims = im.shape
+    if dims[0] != dims[1]:
+        logger.debug('original dims: %s' % str(dims))
+        assert dims[0] % 2 != 0, 'image must have odd dims'
+        assert dims[1] % 2 != 0, 'image must have odd dims'
+
+        dims = np.array(dims)
+        cen = (dims-1)//2
+        cen = cen.astype('i4')
+
+        distances = (
+            cen[0]-0,
+            dims[0]-cen[0]-1,
+            cen[1]-0,
+            dims[1]-cen[1]-1,
+        )
+        logger.debug('distances: %s' % str(distances))
+        min_dist = min(distances)
+
+        start_row = cen[0] - min_dist
+        end_row = cen[0] + min_dist
+        start_col = cen[1] - min_dist
+        end_col = cen[1] + min_dist
+
+        # adding +1 for slices
+        new_im = im[
+            start_row:end_row+1,
+            start_col:end_col+1,
+        ].copy()
+
+        logger.debug('new dims: %s' % str(new_im.shape))
+
+    else:
+        new_im = im
+
+    return new_im
+
+
+def get_ored_bits(maskobj, bitnames):
+    """
+    get or of bits
+
+    Parameters
+    ----------
+    maskobj: lsst mask obj
+        Must have method getPlaneBitMask
+    bitnames: list of strings
+        list of bitmask names
+    """
+    bits = 0
+    for ibit, bitname in enumerate(bitnames):
+        bitval = maskobj.getPlaneBitMask(bitname)
+        bits |= bitval
+
+    return bits
