ded8eb7369b7d74b85251784f72718849951559d
Erin Sheldon <erin.sheldon@gmail.com>
2021-09-13 16:55:37 -0400

    some code cleanup

--- metadetect/lsst_metadetect.py
+++ metadetect/lsst_metadetect.py
@@ -1,235 +1,24 @@
+"""
+
+TODO
+    - add logging
+    - more TODO are in the code
+"""
 import copy
 import numpy as np
-from .base import BaseLSSTMetadetect
 import ngmix
 from ngmix.gexceptions import BootPSFFailure
-import esutil as eu
-import lsst.log
+# import lsst.log
 from lsst.meas.algorithms import KernelPsf
 from lsst.afw.math import FixedKernel
 import lsst.afw.image as afw_image
-from .lsst_medsifier import LSSTMEDSifier
+# from .lsst_medsifier import LSSTMEDSifier
 from .lsst_measure import detect_and_measure, subtract_sky_mbobs
 from . import shearpos
 from .mfrac import measure_mfrac
 from . import procflags
 from . import fitting
 from .defaults import DEFAULT_LOGLEVEL, DEFAULT_MDET_CONFIG
-
-
-class LSSTMetadetect(BaseLSSTMetadetect):
-    """
-    Metadetect for LSST
-
-    Parameters
-    ----------
-    config: dict
-        The configuration
-    mbobs: ngmix.MultiBandObsList
-        The observations
-    rng: np.random.RandomState
-        the random state
-    show: bool, optional
-        If True will show images
-    loglevel: str, optional
-        Defaults to 'INFO'
-    """
-
-    name = 'LSSTMetadetect'
-
-    def __init__(
-        self, config, mbobs, rng,
-        show=False, loglevel=DEFAULT_LOGLEVEL,
-    ):
-
-        self._set_logger(loglevel)
-
-        super().__init__(config=config, mbobs=mbobs, rng=rng, show=show)
-
-        subtract_sky = self.get('subtract_sky', False)
-        if subtract_sky:
-            subtract_sky_mbobs(mbobs=self.mbobs, thresh=self['detect']['thresh'])
-
-    def _set_logger(self, loglevel):
-        self.loglevel = loglevel
-        self.log = lsst.log.Log.getLogger(self.name)
-        self.log.setLevel(getattr(lsst.log, self.loglevel.upper()))
-
-    def _get_all_metacal(self):
-        """
-        get the sheared versions of the observations
-
-        call the parent and then add in the stack exposure with image copied
-        in, modify the variance and set the new psf
-        """
-
-        did_fixnoise = self['metacal'].get('fixnoise', True)
-        orig_mbobs = self.mbobs
-        odict = super()._get_all_metacal()
-        for mtype, mbobs in odict.items():
-            for band in range(len(mbobs)):
-
-                obslist = mbobs[band]
-                orig_obslist = orig_mbobs[band]
-
-                for iobs in range(len(obslist)):
-                    obs = obslist[iobs]
-                    orig_obs = orig_obslist[iobs]
-
-                    exp = copy.deepcopy(orig_obs.coadd_exp)
-                    exp.image.array[:, :] = obs.image
-                    if did_fixnoise:
-                        exp.variance.array[:, :] = exp.variance.array[:, :]*2
-
-                    psf_image = obs.psf.image
-                    stack_psf = KernelPsf(
-                        FixedKernel(
-                            afw_image.ImageD(psf_image.astype(float))
-                        )
-                    )
-                    exp.setPsf(stack_psf)
-                    obs.exposure = exp
-
-                    if self._show:
-                        import descwl_coadd.vis
-                        descwl_coadd.vis.show_image_and_mask(exp)
-                        input('hit a key')
-
-        return odict
-
-    def _do_detect(self, mbobs):
-        return LSSTMEDSifier(
-            mbobs=mbobs,
-            meds_config=self['meds'],
-            thresh=self['detect']['thresh'],
-            loglevel=self.loglevel,
-        )
-
-    def _measure(self, mbobs, shear_str):
-        """
-        perform measurements on the input mbobs. This involves running
-        detection as well as measurements
-        """
-
-        medsifier = self._do_detect(mbobs)
-        mbm = medsifier.get_multiband_meds()
-
-        mbobs_list = mbm.get_mbobs_list()
-
-        res = self._fitter.go(mbobs_list)
-        if res is not None:
-            res = self._add_positions_and_psf(medsifier, res, shear_str, mbobs_list)
-
-        return res
-
-    def _add_positions_and_psf(self, medsifier, res, shear_str, mbobs_list):
-        """
-        TODO add positionis etc.
-        """
-
-        new_dt = [
-            ('box_size', 'i4'),
-            ('row0', 'i4'),  # bbox row start
-            ('col0', 'i4'),  # bbox col start
-            ('row', 'f4'),  # row in image. Use row0 to get to global pixel coords
-            ('col', 'f4'),  # col in image. Use col0 to get to global pixel coords
-            ('row_noshear', 'f4'),  # noshear row
-            ('col_noshear', 'f4'),  # noshear col
-            ('ormask', 'i4'),
-            ('mfrac', 'f4'),
-        ]
-        newres = eu.numpy_util.add_fields(
-            res,
-            new_dt,
-        )
-
-        newres['psfrec_flags'][:] = self.psf_stats['flags']
-        newres['psfrec_g'][:, 0] = self.psf_stats['g1']
-        newres['psfrec_g'][:, 1] = self.psf_stats['g2']
-        newres['psfrec_T'][:] = self.psf_stats['T']
-
-        sources = medsifier.sources
-        det_exp = medsifier.det_exp
-        bbox = det_exp.getBBox()
-
-        for i, rec in enumerate(sources):
-            orig_cen = det_exp.getWcs().skyToPixel(rec.getCoord())
-            if np.isnan(orig_cen.getY()):
-                self.log.debug('falling back on integer location')
-                # fall back to integer pixel location
-                peak = rec.getFootprint().getPeaks()[0]
-                orig_cen = peak.getI()
-
-            # these might be crazy if bbox was odd, but we will
-            # set a flag and bbox will be negative
-            newres['row0'][i] = bbox.getBeginY()
-            newres['col0'][i] = bbox.getBeginX()
-            newres['row'][i] = orig_cen.getY() - newres['row0'][i]
-            newres['col'][i] = orig_cen.getX() - newres['col0'][i]
-
-            if len(mbobs_list[i]) > 0 and len(mbobs_list[i][0]) > 0:
-                newres['box_size'][i] = mbobs_list[i][0][0].image.shape[0]
-
-                newres['ormask'][i] = self.ormask[
-                    int(newres['row'][i]), int(newres['col'][i]),
-                ]
-            else:
-                # the above happens when the bbox is crazy
-                newres['flags'][i] |= procflags.BAD_BBOX
-                newres['box_size'][i] = -9999
-
-        if len(sources) > 0:
-
-            obs = self.mbobs[0][0]
-
-            rows_noshear, cols_noshear = shearpos.unshear_positions(
-                newres['row'],
-                newres['col'],
-                shear_str,
-                obs,  # an example for jacobian and image shape
-                # default is 0.01 but make sure to use the passed in default
-                # if needed
-                step=self['metacal'].get("step", shearpos.DEFAULT_STEP),
-            )
-            newres['row_noshear'] = rows_noshear
-            newres['col_noshear'] = cols_noshear
-
-            if np.any(self.mfrac > 0):
-                # we are using the positions with the metacal shear removed for
-                # this.
-                newres["mfrac"] = measure_mfrac(
-                    mfrac=self.mfrac,
-                    x=newres["col_noshear"],
-                    y=newres["row_noshear"],
-                    box_sizes=newres["box_size"],
-                    obs=obs,
-                    fwhm=self.get("mfrac_fwhm", None),
-                )
-            else:
-                newres["mfrac"] = 0
-
-                # mark bad bboxes as masked as they are when the
-                # measure_mfrac code is run
-                w, = np.where(newres['flags'] & procflags.BAD_BBOX != 0)
-                newres['mfrac'][w] = 1.0
-
-        return newres
-
-    def _set_config(self, config):
-        """
-        set the config, dealing with defaults
-        """
-
-        self.update(config)
-        assert 'metacal' in self, \
-            'metacal setting must be present in config'
-        # assert 'meds' in self, \
-        #     'meds setting must be present in config'
-
-
-#
-# new code
-#
 
 
 def run_metadetect(config, mbobs, rng, show=False, loglevel=DEFAULT_LOGLEVEL):
@@ -540,6 +329,222 @@ def get_stamp_size(meas_type):
 
 
 '''
+class LSSTMetadetect(BaseLSSTMetadetect):
+    """
+    Metadetect for LSST
+
+    Parameters
+    ----------
+    config: dict
+        The configuration
+    mbobs: ngmix.MultiBandObsList
+        The observations
+    rng: np.random.RandomState
+        the random state
+    show: bool, optional
+        If True will show images
+    loglevel: str, optional
+        Defaults to 'INFO'
+    """
+
+    name = 'LSSTMetadetect'
+
+    def __init__(
+        self, config, mbobs, rng,
+        show=False, loglevel=DEFAULT_LOGLEVEL,
+    ):
+
+        self._set_logger(loglevel)
+
+        super().__init__(config=config, mbobs=mbobs, rng=rng, show=show)
+
+        subtract_sky = self.get('subtract_sky', False)
+        if subtract_sky:
+            subtract_sky_mbobs(mbobs=self.mbobs, thresh=self['detect']['thresh'])
+
+    def _set_logger(self, loglevel):
+        self.loglevel = loglevel
+        self.log = lsst.log.Log.getLogger(self.name)
+        self.log.setLevel(getattr(lsst.log, self.loglevel.upper()))
+
+    def _get_all_metacal(self):
+        """
+        get the sheared versions of the observations
+
+        call the parent and then add in the stack exposure with image copied
+        in, modify the variance and set the new psf
+        """
+
+        did_fixnoise = self['metacal'].get('fixnoise', True)
+        orig_mbobs = self.mbobs
+        odict = super()._get_all_metacal()
+        for mtype, mbobs in odict.items():
+            for band in range(len(mbobs)):
+
+                obslist = mbobs[band]
+                orig_obslist = orig_mbobs[band]
+
+                for iobs in range(len(obslist)):
+                    obs = obslist[iobs]
+                    orig_obs = orig_obslist[iobs]
+
+                    exp = copy.deepcopy(orig_obs.coadd_exp)
+                    exp.image.array[:, :] = obs.image
+                    if did_fixnoise:
+                        exp.variance.array[:, :] = exp.variance.array[:, :]*2
+
+                    psf_image = obs.psf.image
+                    stack_psf = KernelPsf(
+                        FixedKernel(
+                            afw_image.ImageD(psf_image.astype(float))
+                        )
+                    )
+                    exp.setPsf(stack_psf)
+                    obs.exposure = exp
+
+                    if self._show:
+                        import descwl_coadd.vis
+                        descwl_coadd.vis.show_image_and_mask(exp)
+                        input('hit a key')
+
+        return odict
+
+    def _do_detect(self, mbobs):
+        return LSSTMEDSifier(
+            mbobs=mbobs,
+            meds_config=self['meds'],
+            thresh=self['detect']['thresh'],
+            loglevel=self.loglevel,
+        )
+
+    def _measure(self, mbobs, shear_str):
+        """
+        perform measurements on the input mbobs. This involves running
+        detection as well as measurements
+        """
+
+        medsifier = self._do_detect(mbobs)
+        mbm = medsifier.get_multiband_meds()
+
+        mbobs_list = mbm.get_mbobs_list()
+
+        res = self._fitter.go(mbobs_list)
+        if res is not None:
+            res = self._add_positions_and_psf(medsifier, res, shear_str, mbobs_list)
+
+        return res
+
+    def _add_positions_and_psf(self, medsifier, res, shear_str, mbobs_list):
+        """
+        TODO add positionis etc.
+        """
+
+        new_dt = [
+            ('box_size', 'i4'),
+            ('row0', 'i4'),  # bbox row start
+            ('col0', 'i4'),  # bbox col start
+            ('row', 'f4'),  # row in image. Use row0 to get to global pixel coords
+            ('col', 'f4'),  # col in image. Use col0 to get to global pixel coords
+            ('row_noshear', 'f4'),  # noshear row
+            ('col_noshear', 'f4'),  # noshear col
+            ('ormask', 'i4'),
+            ('mfrac', 'f4'),
+        ]
+        newres = eu.numpy_util.add_fields(
+            res,
+            new_dt,
+        )
+
+        newres['psfrec_flags'][:] = self.psf_stats['flags']
+        newres['psfrec_g'][:, 0] = self.psf_stats['g1']
+        newres['psfrec_g'][:, 1] = self.psf_stats['g2']
+        newres['psfrec_T'][:] = self.psf_stats['T']
+
+        sources = medsifier.sources
+        det_exp = medsifier.det_exp
+        bbox = det_exp.getBBox()
+
+        for i, rec in enumerate(sources):
+            orig_cen = det_exp.getWcs().skyToPixel(rec.getCoord())
+            if np.isnan(orig_cen.getY()):
+                self.log.debug('falling back on integer location')
+                # fall back to integer pixel location
+                peak = rec.getFootprint().getPeaks()[0]
+                orig_cen = peak.getI()
+
+            # these might be crazy if bbox was odd, but we will
+            # set a flag and bbox will be negative
+            newres['row0'][i] = bbox.getBeginY()
+            newres['col0'][i] = bbox.getBeginX()
+            newres['row'][i] = orig_cen.getY() - newres['row0'][i]
+            newres['col'][i] = orig_cen.getX() - newres['col0'][i]
+
+            if len(mbobs_list[i]) > 0 and len(mbobs_list[i][0]) > 0:
+                newres['box_size'][i] = mbobs_list[i][0][0].image.shape[0]
+
+                newres['ormask'][i] = self.ormask[
+                    int(newres['row'][i]), int(newres['col'][i]),
+                ]
+            else:
+                # the above happens when the bbox is crazy
+                newres['flags'][i] |= procflags.BAD_BBOX
+                newres['box_size'][i] = -9999
+
+        if len(sources) > 0:
+
+            obs = self.mbobs[0][0]
+
+            rows_noshear, cols_noshear = shearpos.unshear_positions(
+                newres['row'],
+                newres['col'],
+                shear_str,
+                obs,  # an example for jacobian and image shape
+                # default is 0.01 but make sure to use the passed in default
+                # if needed
+                step=self['metacal'].get("step", shearpos.DEFAULT_STEP),
+            )
+            newres['row_noshear'] = rows_noshear
+            newres['col_noshear'] = cols_noshear
+
+            if np.any(self.mfrac > 0):
+                # we are using the positions with the metacal shear removed for
+                # this.
+                newres["mfrac"] = measure_mfrac(
+                    mfrac=self.mfrac,
+                    x=newres["col_noshear"],
+                    y=newres["row_noshear"],
+                    box_sizes=newres["box_size"],
+                    obs=obs,
+                    fwhm=self.get("mfrac_fwhm", None),
+                )
+            else:
+                newres["mfrac"] = 0
+
+                # mark bad bboxes as masked as they are when the
+                # measure_mfrac code is run
+                w, = np.where(newres['flags'] & procflags.BAD_BBOX != 0)
+                newres['mfrac'][w] = 1.0
+
+        return newres
+
+    def _set_config(self, config):
+        """
+        set the config, dealing with defaults
+        """
+
+        self.update(config)
+        assert 'metacal' in self, \
+            'metacal setting must be present in config'
+        # assert 'meds' in self, \
+        #     'meds setting must be present in config'
+
+
+#
+# new code
+#
+
+
+
 class LSSTDeblendMetadetect(LSSTMetadetect):
     """
     Metadetect for LSST with deblending at the measurement
