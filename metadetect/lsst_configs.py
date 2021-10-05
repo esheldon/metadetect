e44cb00a42fb4fd483b089a7974763655de3eada
Erin Sheldon <erin.sheldon@gmail.com>
2021-10-05 06:52:50 -0400

    deal with admom

--- metadetect/lsst_configs.py
+++ metadetect/lsst_configs.py
@@ -51,8 +51,9 @@ def _fill_config(config):
     if 'stamp_size' not in config or config['stamp_size'] is None:
         config['stamp_size'] = get_default_stamp_size(meas_type)
 
-    if 'weight' not in config or config['weight'] is None:
-        config['weight'] = get_default_weight_config(meas_type)
+    if config['meas_type'] != 'admom':
+        if 'weight' not in config or config['weight'] is None:
+            config['weight'] = get_default_weight_config(meas_type)
 
     # note we allow ngmix.metacal.get_all_metacal to do its
     # own verification
