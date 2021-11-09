f006b9ba2858f625de6836d01491c893bd65f005
Erin Sheldon <erin.sheldon@gmail.com>
2021-11-09 11:53:52 -0500

    remove debug code

--- metadetect/lsst/measure.py
+++ metadetect/lsst/measure.py
@@ -331,8 +331,6 @@ def get_ormask(source, exposure):
     """
     peak = source.getFootprint().getPeaks()[0]
     orig_cen = peak.getI()
-    import IPython
-    IPython.embed()
     maskval = exposure.mask[orig_cen]
     return maskval
 
