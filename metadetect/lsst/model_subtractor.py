fab634fcd870c81555d1cb1e3ef35b88933d7ac3
Erin Sheldon <erin.sheldon@gmail.com>
2025-12-15 13:37:40 -0500

    add metadetect/lsst/model_subtractor.py

---
+++ metadetect/lsst/model_subtractor.py
@@ -0,0 +1,438 @@
+import logging
+from contextlib import contextmanager
+from lsst.afw.image import MultibandExposure
+import lsst.afw.image as afw_image
+import lsst.geom as geom
+import lsst.afw.detection as afw_det
+from lsst.pex.exceptions import LengthError
+from . import util
+
+LOG = logging.getLogger('lsst_model_subtractor')
+
+
+class ModelSubtractor(object):
+    """
+    Create an image with all models subtracted, which is stored in the .mbexp
+    attribute
+
+    Provides a method to add back a source.  This works in a context manager to
+    maintain data consistency.
+
+    Provides methods to get a postage stamp for a deblended source.  One can
+    also get a stamp for the model or the original image.
+
+    You an also generate the full model of the image.
+
+    Parameters
+    ----------
+    mbexp: lsst.afw.image.MultibandExposure
+        A representation of the multi-band data set.
+        Create one of these with
+            descwl_model_subtractor.get_mbexp(exposure_list)
+    sources: dict of lsst.afw.table.SourceCatalog
+        This is the output of the ScarletDeblendTask, a dict of
+        lsst.afw.table.SourceCatalog keyed by band
+
+    TODO
+    ----
+        - move this to its own repo
+        - This code would use half the memory if heavy footprints supported
+        operations like .addTo(image) or .subtractFrom(image) which should be
+        essentially the same as the code in .insert
+
+    Examples
+    ---------
+
+    subtractor = ModelSubtractor(exposure, sources)
+
+    # add back one model; since the image had data-model this restores the
+    # pixels for the object of interest, but with models of other objects
+    # subtracted
+
+    with subtractor.add_source(source_id):
+        # full MultibandExposure is in subtractor.mbexp
+
+        stamp = subtractor.get_stamp(source_id, stamp_size=48)
+
+    # model of the entire data set as a MultibandExposure
+    full_model = subtractor.get_full_model()
+
+    # model of one source
+    model = subtractor.get_model(source_id, stamp_size=48)
+    """
+
+    def __init__(self, mbexp, sources):
+        assert isinstance(mbexp, MultibandExposure)
+        assert isinstance(sources, dict)
+
+        self.orig = mbexp
+        print(type(self.orig.singles[0]))
+        self.filters = mbexp.filters
+
+        self.sources = sources
+        self.source_ids = set()
+        for source in sources[list(sources.keys())[0]]:
+            self.source_ids.add(source.getId())
+
+        self.mbexp = util.copy_mbexp(mbexp)
+        for band in self.filters:
+            psf = util.try_clone_psf(self.orig[band].getPsf())
+            self.mbexp[band].setPsf(psf)
+
+        # we need a scratch array because heavy footprings don't
+        # have addTo or subtractFrom methods
+        self.scratch = util.copy_mbexp(mbexp, clear=True)
+
+        self._set_footprints()
+        self._build_heavies()
+        self._build_subtracted_image()
+
+    @contextmanager
+    def add_source(self, source_id):
+        """
+        Open a with context that yields the image with all objects
+        subtracted except the specified one.
+
+        since the image had data-model this restores the pixels for the object
+        of interest, minus models of other objects
+
+        with subtractor.add_source(source_id):
+            # do something with subtractor.mbexp
+
+        Parameters
+        ----------
+        source_id: int
+            The id of the source, e.g. from source.getId()
+
+        Yields
+        -------
+        ExposureF, although more typically one uses the .mbexp attribute
+        """
+        if source_id not in self.source_ids:
+            raise ValueError(f'source {source_id} is not in the source list')
+
+        self._add_or_subtract_source(source_id, 'add')
+        # self._add_or_subtract_source_new(source_id, 'add')
+        try:
+            yield self.mbexp
+        finally:
+            self._add_or_subtract_source(source_id, 'subtract')
+            # self._add_or_subtract_source_new(source_id, 'subtract')
+
+    def _add_or_subtract_source(self, source_id, type):
+        mbexp = self.mbexp
+        scratch = self.scratch
+
+        bbox = self.get_bbox(source_id)
+
+        for band in self.filters:
+            # Because footprints can only be used to *replace* pixels, we do so
+            # on a scratch image and then subtract that from the model image
+
+            heavy_fp = self.heavies[band][source_id]
+            heavy_fp.insert(scratch[band].image)
+
+            if type == 'add':
+                mbexp[band].image[bbox] += scratch[band].image[bbox]
+            else:
+                mbexp[band].image[bbox] -= scratch[band].image[bbox]
+
+            scratch[band].image[bbox] = 0
+
+    def _add_or_subtract_source_new(self, source_id, type):
+        mbexp = self.mbexp
+        scratch = self.scratch
+
+        bbox = self.get_bbox(source_id)
+
+        for band in self.filters:
+            # Because footprints can only be used to *replace* pixels, we do so
+            # on a scratch image and then subtract that from the model image
+
+            heavy_fp = self.heavies[band][source_id]
+            heavy_fp.insert(scratch[band].image)
+
+            if type == 'add':
+                heavy_fp.addTo(mbexp[band].image[bbox])
+            else:
+                heavy_fp.subtractFrom(mbexp[band].image[bbox])
+
+            scratch[band].image[bbox] = 0
+
+    def get_stamp(
+        self,
+        source_id,
+        stamp_size=None,
+        clip=False,
+        type='deblended',
+    ):
+        """
+        Get a postage stamp exposure at the location of the specified source.
+        The pixel data are copied.
+
+        If you want the object to be in the image, use this method within
+        an add_source context
+
+        with subtractor.add_source(source_id):
+            stamp = subtractor.get_stamp(source_id)
+
+        Parameters
+        ----------
+        source_id: int
+            The id of the source, e.g. from source.getId()
+        stamp_size: int
+            If sent, a bounding box is created with about this size rather than
+            using the footprint bounding box. Typically the returned size is
+            stamp_size + 1
+        clip: bool, optional
+            If set to True, clip the bbox to fit into the exposure.
+
+            If clip is False and the bbox does not fit, a
+            lsst.pex.exceptions.LengthError is raised
+
+            Only relevant if stamp_size is sent.  Default False
+        type: str, optional
+            'deblended', 'original', 'model'.  Default is 'deblended'.
+
+            'deblended' means whatever is in the current subtracted images.
+                If the user is in the add_source() context it will contain
+                the source data because the model will have been added back in
+
+            'original' means a stamp from the original data
+
+            'model' means the model for object
+                You can also use get_model() to get the model
+
+        Returns
+        -------
+        ExposureF
+        """
+
+        assert type in ['deblended', 'original', 'model'], (
+            'type must be one of deblended, original or model'
+        )
+
+        if type == 'model':
+            return self.get_model(source_id, stamp_size=stamp_size, clip=clip)
+
+        if source_id not in self.source_ids:
+            raise ValueError(f'source {source_id} is not in the source list')
+
+        bbox = self.get_bbox(source_id, stamp_size=stamp_size, clip=clip)
+
+        if type == 'original':
+            mbexp = self.orig
+        else:
+            mbexp = self.mbexp
+
+        exposures = [mbexp[band][bbox] for band in self.filters]
+        # return MultibandExposure.fromExposures(self.filters, exposures)
+        return util.get_mbexp(exposures)
+
+    def get_model(self, source_id, stamp_size=None, clip=False):
+        """
+        Get a postage stamp exposure at the location of the specified source,
+        containing the model rather than data.
+
+        Parameters
+        ----------
+        source_id: int
+            The id of the source, e.g. from source.getId()
+        stamp_size: int
+            If sent, a bounding box is created with about this size rather than
+            using the footprint bounding box. Typically the returned size is
+            stamp_size + 1
+        clip: bool, optional
+            If set to True, clip the bbox to fit into the exposure.
+
+            If clip is False and the bbox does not fit, a
+            lsst.pex.exceptions.LengthError is raised
+
+            Only relevant if stamp_size is sent.  Default False
+
+        Returns
+        -------
+        ExposureF
+        """
+
+        if source_id not in self.source_ids:
+            raise ValueError(f'source {source_id} is not in the source list')
+
+        scratch = self.scratch
+        heavies = self.heavies
+
+        bbox = self.get_bbox(source_id, stamp_size=stamp_size, clip=clip)
+
+        exposures = []
+        for band in self.filters:
+            heavy_fp = heavies[band][source_id]
+            heavy_fp.insert(scratch[band].image)
+
+            model_exp = afw_image.ExposureF(scratch[band][bbox], deep=True)
+
+            scratch[band].image[bbox] = 0
+
+            exposures.append(model_exp)
+
+        # return MultibandExposure.fromExposures(self.filters, exposures)
+        return util.get_mbexp(exposures)
+
+    def get_full_model(self):
+        """
+        Get a full model image of all sources.
+
+        Returns
+        -------
+        ExposureF
+        """
+        heavies = self.heavies
+        scratch = self.scratch
+
+        model = util.copy_mbexp(self.mbexp, clear=True)
+
+        for band, sources in self.sources.items():
+            LOG.debug('-' * 70)
+            LOG.debug(f'band: {band}')
+
+            parents = sources.getChildren(0)
+            for parent_record in parents:
+                LOG.debug('parent id: %d', parent_record.getId())
+
+                children = sources.getChildren(parent_record.getId())
+                nchild = len(children)
+                LOG.debug(
+                    'processing %d %s',
+                    nchild,
+                    'children' if nchild > 1 else 'child',
+                )
+
+                for child in children:
+                    child_id = child.getId()
+                    heavy_fp = heavies[band][child_id]
+                    heavy_fp.insert(scratch[band].image)
+
+                    bbox = self.get_bbox(child_id)
+                    model[band].image[bbox] += scratch[band].image[bbox]
+                    scratch[band].image[bbox] = 0
+
+        return model
+
+    def get_bbox(self, source_id, stamp_size=None, clip=False):
+        """
+        Get a bounding box at the location of the specified source.
+
+        Parameters
+        ----------
+        source_id: int
+            The id of the source, e.g. from source.getId()
+        stamp_size: int
+            If sent, a bounding box is created with about this size rather than
+            using the footprint bounding box. Typically the returned size is
+            stamp_size + 1
+        clip: bool, optional
+            If set to True, clip the bbox to fit into the exposure.
+
+            If clip is False and the bbox does not fit, a
+            lsst.pex.exceptions.LengthError is raised
+
+            Only relevant if stamp_size is sent.  Default False
+
+        Returns
+        -------
+        lsst.geom.Box2I
+        """
+
+        if source_id not in self.source_ids:
+            raise ValueError(f'source {source_id} is not in the source list')
+
+        # assumption: bounding boxes same in all bands
+        band = self.filters[0]
+
+        if stamp_size is not None:
+            parent_id, fp = self.footprints[band][source_id]
+            peak = fp.getPeaks()[0]
+
+            # note we previously had -0.5 on each of these based on Bob's code
+            x_peak, y_peak = peak.getIx(), peak.getIy()
+
+            bbox = geom.Box2I(
+                geom.Point2I(x_peak, y_peak),
+                geom.Extent2I(1, 1),
+            )
+            bbox.grow(stamp_size // 2)
+
+            exp_bbox = self.mbexp.getBBox()
+            if clip:
+                bbox.clip(exp_bbox)
+            else:
+                if not exp_bbox.contains(bbox):
+                    raise LengthError(
+                        f'requested stamp size {stamp_size} for source '
+                        f'{source_id} does not fit into the exposoure.  '
+                        f'Use clip=True to clip the bbox to fit'
+                    )
+
+        else:
+            parent_id, fp = self.footprints[band][source_id]
+            bbox = fp.getBBox()
+
+        return bbox
+
+    def _set_footprints(self):
+        self.footprints = {}
+        for band, sources in self.sources.items():
+            self.footprints[band] = {
+                source.getId(): (source.getParent(), source.getFootprint())
+                for source in sources
+            }
+
+    def _build_heavies(self):
+        self.heavies = {}
+        for band, footprints in self.footprints.items():
+            self.heavies[band] = {}
+
+            for id, fp in footprints.items():
+                # TODO test/ask if this is ok
+                self.heavies[band][id] = afw_det.makeHeavyFootprint(
+                    fp[1],
+                    self.mbexp[band].maskedImage,
+                )
+
+                # if fp[1].isHeavy():
+                #     self.heavies[band][id] = fp[1]
+                # elif fp[0] == 0:
+                #     self.heavies[band][id] = afw_det.makeHeavyFootprint(
+                #         fp[1], self.mbexp[band].maskedImage,
+                #     )
+
+    def _build_subtracted_image(self):
+        heavies = self.heavies
+        mbexp = self.mbexp
+        scratch = self.scratch
+
+        for band, sources in self.sources.items():
+            LOG.debug('-' * 70)
+            LOG.debug(f'band: {band}')
+
+            parents = sources.getChildren(0)
+            for parent_record in parents:
+                LOG.debug('parent id: %d', parent_record.getId())
+
+                children = sources.getChildren(parent_record.getId())
+                nchild = len(children)
+                LOG.debug(
+                    'processing %d %s',
+                    nchild,
+                    'children' if nchild > 1 else 'child',
+                )
+
+                for child in children:
+                    child_id = child.getId()
+                    heavy_fp = heavies[band][child_id]
+                    print('heavy_fp type:', type(heavy_fp))
+                    heavy_fp.insert(scratch[band].image)
+
+                    bbox = self.get_bbox(source_id=child_id)
+                    # mbexp[band].image[bbox] -= scratch[band].image[bbox]
+                    mbexp[band].image[bbox] -= scratch[band].image[bbox]
+                    scratch[band].image[bbox] = 0
