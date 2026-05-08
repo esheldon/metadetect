BUG shear bands not used for averaging original PSF quantities
We now send shear bands to fit_original_psfs_mbexp
We keep track in the perband structure of what was averaged

this perband stats structure is returned by the task as a
dict entry

metadetect/lsst/metadetect.py
@@ -217,6 +217,7 @@ class MetadetectTask(Task):
 
         psf_stats = fit_original_psfs_mbexp(
             mbexp=mbexp,
+            config=config,
             wgts=wgts,
             rng=rng,
         )
@@ -254,6 +255,7 @@ class MetadetectTask(Task):
 
             result[shear_str] = res
 
+        result['perband_psf_stats'] = psf_stats['perband']
         return result
 
 
@@ -437,6 +439,7 @@ def add_original_psf(psf_stats, res):
     copy in psf results
     """
     res['psfrec_flags'][:] = psf_stats['flags']
+    res['psfrec_navg'][:] = psf_stats['navg']
     res['psfrec_g'][:, 0] = psf_stats['g1']
     res['psfrec_g'][:, 1] = psf_stats['g2']
     res['psfrec_T'][:] = psf_stats['T']
@@ -501,19 +504,46 @@ def get_mfrac_mbexp(mbexp, mfrac_mbexp):
     return mfrac, wgts
 
 
-def fit_original_psfs_mbexp(mbexp, rng, wgts):
+def fit_original_psfs_mbexp(mbexp, rng, wgts, config):
     """
-    fit the original psfs at the center of the image and get the mean g1,g2,T
-    across all bands
+    fit the original psfs at the center of the image.
+
+    also get the mean g1,g2,T across shear bands
 
     This can fail and flags will be set, but we proceed
+
+    Parameters
+    ----------
+    mbexp: MutiBandExposure
+        The image data
+    rng: np.random.RandomState
+        The numpy random state
+    wgts: sequence
+        Sequence of weights, one per image
+    config: dict like
+        Should have the metadetect config into
+
+    Returns
+    -------
+    dict with entries
+        flags: Flags for processing
+        navg: Number of images averaged, should be same size as shear bands
+        e1: mean e1 (distortion style)
+        e2: mean e2 (distortion style)
+        g1: mean g1 (reduced shear style)
+        g2: mean g2 (reduced shear style)
+        T: Mean trace of the covariance matrix
+        perband: array with results for each band separately. this
+            includes non shear bands.
     """
+
+    shear_band_indices = util.extract_shear_band_indices(
+        mbexp=mbexp,
+        config=config,
+    )
 
     nband = len(mbexp)
     assert len(wgts) == nband
-    wsum = sum(wgts)
-    if wsum <= 0:
-        raise ValueError(f'got sum(wgts) = {wsum}')
 
     fitter = ngmix.admom.AdmomFitter(rng=rng)
     guesser = ngmix.guessers.GMixPSFGuesser(
@@ -524,15 +554,26 @@ def fit_original_psfs_mbexp(mbexp, rng, wgts):
     runner = ngmix.runners.PSFRunner(fitter=fitter, guesser=guesser, ntry=4)
 
     perband = np.zeros(
-        nband, dtype=[('e1', 'f8'), ('e2', 'f8'), ('T', 'f8')]
+        nband,
+        dtype=[
+            ('e1', 'f8'),
+            ('e2', 'f8'),
+            ('wgt', 'f8'),
+            ('T', 'f8'),
+            ('is_shear_band', bool),
+        ]
     )
 
     try:
+        # these are only summed over shear_bands
+        navg = 0
+        wsum = 0.0
         e1sum = 0.0
         e2sum = 0.0
         Tsum = 0.0
 
         for iband, exp, wgt in zip(range(nband), mbexp, wgts):
+
             cen, _ = util.get_integer_center(
                 wcs=exp.getWcs(),
                 bbox=exp.getBBox(),
@@ -557,13 +598,21 @@ def fit_original_psfs_mbexp(mbexp, rng, wgts):
             e1, e2 = res['e']
             T = res['T']
 
-            e1sum += e1 * wgt
-            e2sum += e2 * wgt
-            Tsum += T * wgt
-
             perband['e1'][iband] = e1
             perband['e2'][iband] = e2
             perband['T'][iband] = T
+            perband['wgt'][iband] = wgt
+
+            if iband in shear_band_indices:
+                perband['is_shear_band'][iband] = True
+                navg += 1
+                wsum += wgt
+                e1sum += e1 * wgt
+                e2sum += e2 * wgt
+                Tsum += T * wgt
+
+        if wsum <= 0:
+            raise ValueError(f'got sum(wgts) = {wsum}')
 
         e1 = e1sum / wsum
         e2 = e2sum / wsum
@@ -581,8 +630,13 @@ def fit_original_psfs_mbexp(mbexp, rng, wgts):
 
         perband = None
 
+    assert navg == len(shear_band_indices), (
+        f'summed {navg} but expected {len(shear_band_indices)}'
+    )
+
     return {
         'flags': flags,
+        'navg': navg,
         'e1': e1,
         'e2': e2,
         'g1': g1,

metadetect/lsst/tests/test_lsst_metadetect.py
@@ -42,23 +42,32 @@ def make_lsst_sim(
         hlr=hlr,
     )
 
-    if psf_type == 'ps':
-        psf = descwl_shear_sims.psfs.make_ps_psf(
-            rng=rng,
-            dim=300,
-        )
-    else:
-        psf = descwl_shear_sims.psfs.make_fixed_psf(psf_type=psf_type)
+    # This way we get different PSFs per band for PS PSF
+    sim_data = {'band_data': {}}
+    for band in bands:
+        if psf_type == 'ps':
+            psf = descwl_shear_sims.psfs.make_ps_psf(
+                rng=rng,
+                dim=300,
+            )
+        else:
+            psf = descwl_shear_sims.psfs.make_fixed_psf(psf_type=psf_type)
 
-    sim_data = descwl_shear_sims.make_sim(
-        rng=rng,
-        galaxy_catalog=galaxy_catalog,
-        coadd_dim=coadd_dim,
-        g1=0.02,
-        g2=0.00,
-        psf=psf,
-        bands=bands,
-    )
+        tsim_data = descwl_shear_sims.make_sim(
+            rng=rng,
+            galaxy_catalog=galaxy_catalog,
+            coadd_dim=coadd_dim,
+            g1=0.02,
+            g2=0.00,
+            psf=psf,
+            bands=bands,
+        )
+
+        for key in tsim_data:
+            if key == 'band_data':
+                sim_data['band_data'][band] = tsim_data['band_data'][band]
+            else:
+                sim_data[key] = tsim_data[key]
     return sim_data
 
 
@@ -221,11 +230,13 @@ def test_lsst_metadetect_shear_bands():
     rng = np.random.RandomState(seed=116)
 
     bands = ['g', 'r', 'i', 'z']
+    shear_bands = ["r", "z"]
+
     nband = len(bands)
-    sim_data = make_lsst_sim(116, bands=bands)
+    sim_data = make_lsst_sim(116, bands=bands, psf_type='ps')
     data = do_coadding(rng=rng, sim_data=sim_data, nowarp=True)
 
-    config = {"shear_bands": ["r", "z"]}
+    config = {"shear_bands": shear_bands}
     metacal_types = ['noshear', '1p', '1m']
 
     detected = afw_image.Mask.getPlaneBitMask('DETECTED')
@@ -254,15 +265,45 @@ def test_lsst_metadetect_shear_bands():
             assert np.all(res[shear]["mfrac"] == 0)
             assert res[shear][flux_name].shape == (25, nband)
 
+    perband = res['perband_psf_stats']
+    assert perband.size == len(bands)
+    shear_band_indices, = np.where(perband['is_shear_band'])
+    assert shear_band_indices.size == len(shear_bands)
+
+    print(perband['e1'])
+
+    wsum = perband['wgt'][shear_band_indices].sum()
+    e1sum = perband['e1'][shear_band_indices].sum()
+    e2sum = perband['e2'][shear_band_indices].sum()
+    Tsum = perband['T'][shear_band_indices].sum()
+    e1 = e1sum / wsum
+    e2 = e2sum / wsum
+    g1, g2 = ngmix.shape.e1e2_to_g1g2(e1, e2)
+    T = Tsum / wsum
+
     for shear in metacal_types:
         assert np.all(res[shear]["shear_bands"] == np.array([["13"]]))
+
         # g and i band should be all NaNs for gauss
         assert np.all(np.isnan(res[shear]["gauss_band_flux"][:, 0]))
         assert np.all(np.isnan(res[shear]["gauss_band_flux"][:, 2]))
+
         # rest should be finite
         assert np.all(np.isfinite(res[shear]["gauss_band_flux"][:, 1]))
         assert np.all(np.isfinite(res[shear]["gauss_band_flux"][:, 3]))
         assert np.all(np.isfinite(res[shear]["pgauss_band_flux"]))
+
+        assert np.all(res[shear]['psfrec_navg'] == len(shear_bands))
+
+        np.testing.assert_allclose(
+            res[shear]['psfrec_g'][:, 0], g1,
+        )
+        np.testing.assert_allclose(
+            res[shear]['psfrec_g'][:, 1], g2,
+        )
+        np.testing.assert_allclose(
+            res[shear]['psfrec_T'], T,
+        )
 
 
 def test_lsst_metadetect_pgauss():