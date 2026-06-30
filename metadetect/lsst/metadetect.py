ce8445928215d7869768c518f5797187f545dff7
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2026-06-30 01:11:45 -0400

    Retarget to Scarlet task, optionally

--- metadetect/lsst/metadetect.py
+++ metadetect/lsst/metadetect.py
@@ -3,6 +3,9 @@ import numpy as np
 import ngmix
 
 from ngmix.gexceptions import BootPSFFailure, GMixRangeError
+from copy import deepcopy
+from frozendict import frozendict
+
 from lsst.pex.config import (
     Config,
     ConfigField,
@@ -14,6 +17,7 @@ from lsst.pex.config import (
 )
 from lsst.pipe.base import Task
 from lsst.meas.algorithms import SourceDetectionTask
+from lsst.meas.extensions.scarlet import ScarletDeblendTask
 
 from .. import procflags
 
@@ -21,7 +25,7 @@ from .skysub import subtract_sky_mbexp
 
 from .defaults import (
     DEFAULT_DEBLEND_SCARLET_CONFIG,
-    DEFAULT_DEBLEND_SDSS_CONFIG,
+    DEFAULT_MDET_CONFIG,
     DEFAULT_STAMP_SIZE,
     DEFAULT_SUBTRACT_SKY,
     DEFAULT_PGAUSS_FWHM,
@@ -97,11 +101,14 @@ def run_metadetect(
 
     if deblender == 'scarlet':
         # Load the default scarlet config
-        config_override['deblend'] = deepcopy(DEFAULT_DEBLEND_SCARLET_CONFIG)
-        assert config_override['deblend'].pop('name') == 'scarlet'
+        config_override['detect_and_deblend']['deblend'] = deepcopy(
+            DEFAULT_DEBLEND_SCARLET_CONFIG
+        )
+        assert config_override['detect_and_deblend']['deblend'].pop('name') == 'scarlet'
+        config.detect_and_deblend.deblend.retarget(ScarletDeblendTask)
     elif deblender == 'sdss':
         # SDSS deblender is the default, so no need to override
-        assert config_override['deblend'].pop('name') == 'sdss'
+        assert config_override['detect_and_deblend']['deblend'].pop('name') == 'sdss'
     else:
         raise ValueError(f"Unknown deblender: {deblender}")
 
@@ -176,7 +183,7 @@ class MetadetectConfig(Config):
 
     detect_and_deblend = ConfigurableField(
         doc="Detection and Deblending config",
-        target=DetectAndDeblendTask,
+        target=measure.DetectAndDeblendTask,
     )
 
     metacal = ConfigField[MetacalConfig](
@@ -224,8 +231,7 @@ class MetadetectTask(Task):
         border=0,
     ):
         # This is to support methods that are not yet refactored.
-        config = self.config.toDict()
-        config['detect']['thresh'] = self.detect.config.thresholdValue
+        config = frozendict(self.config.toDict())
 
         shear_bands = config['shear_bands'] or mbexp.bands
         if not all(band in mbexp.bands for band in shear_bands):
